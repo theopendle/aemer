7e68c2ef3c81753f65a623f0bac778ba6f8c9447
tpendle <theopendle@gmail.com>
2018-08-09 20:43:10 +0200

    Improved check for AEM indicators

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,10 @@
+const ETC_DESIGNS = '/etc/designs';
+
 var html = document.documentElement.innerHTML;
-var aem = html.includes('/etc/designs');
+
+var links = $('link[href*="' + ETC_DESIGNS + '"]');
+var scripts = $('script[src*="' + ETC_DESIGNS + '"]');
+var aem = links.length > 0 || scripts.length > 0;
 
 if(aem) {
   console.log("Aemer: This website uses AEM.");
