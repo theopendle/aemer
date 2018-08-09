710ce1abed7f0c81d2ec1d84bbab0d4be4a960e8
tpendle <theopendle@gmail.com>
2018-08-09 18:00:09 +0200

    Styled scrollbar and fixed links

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,7 +5,7 @@ $('document').ready(function(){
     if(!!result[AEM_URLS] && result[AEM_URLS].length > 0) {
       var sites = result[AEM_URLS];
       for (var i = 0; i < sites.length; i++) {
-        $('#site-list').append('<li><a href="http://' + sites[i] + '">' + sites[i] + '</a></li>');
+        $('#site-list').append('<li><a href="http://' + sites[i] + '" target="_blank">' + sites[i] + '</a></li>');
       }
       $('#site-list').removeAttr('hidden');
       $('#no-list').attr('hidden', '');
