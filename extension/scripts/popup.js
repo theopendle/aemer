9a8b97b94a0fb0220b3bcb4c561629962607ee62
tpendle <theopendle@gmail.com>
2018-08-15 09:32:53 +0200

    Added upload and download to/from CSV

--- extension/scripts/popup.js
+++ extension/scripts/popup.js
@@ -1,11 +1,22 @@
 const AEM_URLS = 'aemUrls';
+var siteList;
 
-$('document').ready(function(){
+function init() {
+  $('document').ready(function(){
+    refreshList();
+
+    $('#upload-input').change(function(e) {
+      doUpload();
+    });
+  });
+}
+
+function refreshList() {
   chrome.storage.sync.get([AEM_URLS], function(result) {
-    if(!!result[AEM_URLS] && result[AEM_URLS].length > 0) {
-      var sites = result[AEM_URLS];
-      for (var i = 0; i < sites.length; i++) {
-        $('#site-list').append('<li><a href="http://' + sites[i] + '" target="_blank">' + sites[i] + '</a></li>');
+    siteList = !!result && !!result[AEM_URLS] ? result[AEM_URLS] : [];
+    if(siteList.length > 0) {
+      for (var i = 0; i < siteList.length; i++) {
+        $('#site-list').append('<li><a href="http://' + siteList[i] + '" target="_blank">' + siteList[i] + '</a></li>');
       }
       $('#sites').removeAttr('hidden');
       $('#no-sites').attr('hidden', '');
@@ -14,5 +25,55 @@ $('document').ready(function(){
       $('#sites').attr('hidden', '');
       $('#no-sites').removeAttr('hidden');
     }
+    initDownload();
   });
-});
+}
+
+function initDownload() {
+  var sitesString = encodeURIComponent(siteList.join(','));
+  $('#download-link').each(function() {
+    $(this).attr('href', $(this).attr('href') + sitesString);
+  });
+}
+
+function doUpload() {
+  var input = $('#upload-input')[0];
+  if(!!input.files) {
+    if(input.files.length < 1) {
+      alert('Please select a file.');
+    } else {
+      var file = input.files[0];
+      readFileContent(file).then(function(content) {
+        var importList = content.split(',');
+        saveSites(importList);
+      });
+    }
+  }
+}
+
+function readFileContent(file) {
+	const reader = new FileReader()
+  return new Promise((resolve, reject) => {
+    reader.onload = event => resolve(event.target.result)
+    reader.onerror = error => reject(error)
+    reader.readAsText(file)
+  })
+}
+
+function saveSites(urlList){
+  chrome.storage.sync.get([AEM_URLS], function(result) {
+    var array = (result[AEM_URLS]) ? result[AEM_URLS] : [];
+    for (var i = 0; i < urlList.length; i++) {
+      var site = urlList[i];
+      if(!array.includes(site)) {
+        array.push(site);
+      }
+    }
+    array.sort();
+    chrome.storage.sync.set({ "aemUrls": array }, function() {
+      refreshList();
+    });
+  });
+}
+
+init();
